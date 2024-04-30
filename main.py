bfc3259b00ec6613d485e72c3374abaf87e98da5
nox <alexandrearthurpg@gmail.com>
2024-04-30 14:53:25 -0300

    base create

--- main.py
+++ main.py
@@ -75,6 +75,7 @@ class Login(UserControl):
         self.main = MainPage()
 
         self.page.controls.insert(0, self.main)
+
         self.page.update()
         sleep(0.35)
 
@@ -305,7 +306,7 @@ class Cashier(UserControl):
             height=HEIGHT_SCREEN,
             bgcolor=BG_COLOR,
             animate=animation.Animation(550, AnimationCurve.EASE_IN_OUT),
-         
+
         )
 
         self.cashier_box = Row(
@@ -345,14 +346,15 @@ class Cashier(UserControl):
             ]
         )
 
-
         self.new_cashier = Column(
             alignment=MainAxisAlignment.END,
             horizontal_alignment=CrossAxisAlignment.CENTER,
             controls=[Row(
                 controls=[
-                    self.btn_cashier("Voltar", lambda x: self.close_modal(), icon=icons.ARROW_BACK_IOS_NEW_OUTLINED),
-                    self.btn_cashier("Adicionar Caixa", lambda x: print("Adicionar Caixa"), icon=icons.PERSON_ADD),
+                    self.btn_cashier("Voltar", lambda x: self.close_modal(
+                    ), icon=icons.ARROW_BACK_IOS_NEW_OUTLINED),
+                    self.btn_cashier("Adicionar Caixa", lambda x: print(
+                        "Adicionar Caixa"), icon=icons.PERSON_ADD),
 
                 ],
                 vertical_alignment=CrossAxisAlignment.CENTER,
@@ -412,8 +414,11 @@ class Cashier(UserControl):
         self.cashier.update()
         sleep(0.75)
 
+        self.page.controls.remove(self)
+
         self.main = MainPage()
         self.page.controls.insert(0, self.main)
+
         self.page.update()
 
         self.main.open_mainpage()
@@ -531,7 +536,7 @@ class Cashier(UserControl):
 
 class Product(UserControl):
     def __init__(self):
-        
+
         self.page_number = 0
         self.page_size = 30
 
@@ -543,10 +548,56 @@ class Product(UserControl):
             animate=animation.Animation(550, AnimationCurve.EASE_IN_OUT),
         )
 
-        self.product_box = Row()
+        self.product_box = Row(
+            alignment=MainAxisAlignment.CENTER,
+            spacing=20,
+            opacity=100,
+            animate_opacity=800,
+            visible=False
+        )
 
-        self.btn = self.product_btn("Teste", lambda e: (
-            self.close_modal()), icon_selected=icons.ABC)
+        self.title = Text('Lista de Produtos',
+                          style=TextStyle(16, weight='bold'))
+
+        self.new_product = Row(
+            controls=[
+                self.product_btn('Voltar', lambda e: self.close_modal(
+                ), icon_selected=icons.ARROW_BACK_IOS),
+                self.product_btn('Adicionar', lambda e: print(
+                    'Adicionar produto'), icon_selected=icons.NOTE_ADD)
+            ],
+            vertical_alignment=CrossAxisAlignment.CENTER,
+            alignment=MainAxisAlignment.SPACE_AROUND,
+        )
+
+        self.product_view = ListView(expand=True, spacing=20, padding=10,
+                                     controls=[DataTable(
+                                         expand=True,
+                                         border_radius=8,
+                                         border=border.all(2, TEXT_COLOR),
+                                         horizontal_lines=BorderSide(1),
+                                         columns=[
+                                             DataColumn(Text('COD')),
+                                             DataColumn(Text('PRODUTO')),
+                                             DataColumn(Text('OBSERVAÇÕES')),
+                                             DataColumn(Text('PREÇO')),
+                                             DataColumn(Text('ESTOQUE')),
+                                         ],
+                                         rows=[
+
+                                         ]
+                                     )])
+        self.btns = Row(
+            alignment=MainAxisAlignment.SPACE_AROUND,
+            controls=[
+                self.product_btn("Anterior", lambda x: self.move_page(
+                    "previous"), icon_selected=icons.ARROW_BACK),
+                self.product_btn('Próximo', lambda x: self.move_page(
+                    'next'), icon_selected=icons.ARROW_FORWARD)
+            ],
+
+        )
+        self.btns.controls[0].disabled = True
 
         super().__init__()
 
@@ -567,8 +618,12 @@ class Product(UserControl):
         self.product.update()
         sleep(0.75)
 
+        self.page.controls.remove(self)
+
         self.main = MainPage()
         self.page.controls.insert(0, self.main)
+
+        print(self.page.controls)
         self.page.update()
         sleep(0.35)
 
@@ -585,24 +640,157 @@ class Product(UserControl):
             icon=icon_selected
         )
 
+    def move_page(self, direction: str):
+
+        if direction == 'next':
+            self.page_number += 1
+
+        elif direction == 'previous':
+            self.page_number -= 1
+
+        if self.page_number <= 0:
+
+            self.page_number = 0
+            # Previous Btn
+            self.btns.controls[0].disabled = True
+            self.btns.controls[0].update()
+
+        elif self.page_number >= 5:
+            self.btns.controls[1].disabled = True
+            self.btns.controls[1].update()
+
+        else:
+            # Previous Btn
+            self.btns.controls[0].disabled = False
+            self.btns.controls[0].update()
+
+            # Next Btn
+            self.btns.controls[1].disabled = False
+            self.btns.controls[1].update()
+
+        start_index = self.page_size*self.page_number
+
+        self.product_view.controls[0].rows.clear()
+
+        for i in range(self.page_size):
+            row = DataRow(
+                cells=[
+                    DataCell(Text(start_index+i)),
+                    DataCell(Text("Carro")),
+                    DataCell(Text('4 portas')),
+                    DataCell(Text('R$ 100',)),
+                    DataCell(Text('1000 und')),
+                ]
+            )
+            self.product_view.controls[0].rows.append(row)
+
+        self.product_view.update()
+
     def build(self):
 
-        self.product_box.controls.append(self.btn)
+        container = Column(
+            expand=True,
+            horizontal_alignment=CrossAxisAlignment.CENTER,
+            alignment=MainAxisAlignment.CENTER
+        )
+
+        container.controls.append(self.title)
+        container.controls.append(self.new_product)
+        container.controls.append(self.product_view)
+        container.controls.append(self.btns)
+
+        for i in range(self.page_size):
+            row = DataRow(
+                cells=[
+                    DataCell(Text(i+1)),
+                    DataCell(Text("Carro")),
+                    DataCell(Text('4 portas')),
+                    DataCell(Text('R$ 100',)),
+                    DataCell(Text('1000 und')),
+                ]
+            )
+
+            self.product_view.controls[0].rows.append(row)
+
+        self.product_box.controls.append(container)
         self.product.content = self.product_box
         return self.product
 
 
 class Order(UserControl):
     def __init__(self):
+
+        self.page_size: int = 30
+        self.page_number: int = 0
+
         self.order = Container(
             padding=10,
             width=0,
             height=HEIGHT_SCREEN,
-            bgcolor="yellow",
+            bgcolor=BG_SUB_COLOR,
             animate=animation.Animation(550, AnimationCurve.EASE_IN_OUT),
         )
 
-        self.order_box = Row()
+        self.order_box = Row(
+            alignment=MainAxisAlignment.CENTER,
+            vertical_alignment=CrossAxisAlignment.CENTER,
+            spacing=20,
+            opacity=100,
+            animate_opacity=800,
+            visible=False,
+        )
+
+        self.title = Text('Lista de Pedidos',
+                          style=TextStyle(16, weight='bold'))
+
+        self.new_order = Row(
+            alignment=MainAxisAlignment.SPACE_AROUND,
+            vertical_alignment=CrossAxisAlignment.CENTER,
+            controls=[
+                self.order_btn("Voltar", lambda e:  self.close_modal(),
+                               icon_selected=icons.ARROW_BACK),
+                self.order_btn('Adicionar', lambda e: print(
+                    'Adicionar Pedido'), icon_selected=icons.ADD_CARD)
+            ],
+        )
+
+        self.order_list = ListView(
+            expand=True,
+            spacing=20,
+            padding=10,
+            controls=[
+                DataTable(
+                    expand=True,
+                    border_radius=8,
+                    border=border.all(2, TEXT_COLOR),
+                    horizontal_lines=BorderSide(1),
+                    columns=[
+                        DataColumn(Text('ID')),
+                        DataColumn(Text('DIA')),
+                        DataColumn(Text('CLIENTE')),
+                        DataColumn(Text('CAIXA')),
+                        DataColumn(Text('PAGAMENTO')),
+                        DataColumn(Text('LISTA DE PRODUTOS')),
+
+                    ],
+                    rows=[
+
+                    ]
+                )
+            ]
+        )
+
+        self.btns = Row(
+            alignment=MainAxisAlignment.SPACE_AROUND,
+            controls=[
+                self.order_btn("Anterior", lambda x: self.move_page(
+                    "previous"), icon_selected=icons.ARROW_BACK),
+                self.order_btn('Próximo', lambda x: self.move_page(
+                    'next'), icon_selected=icons.ARROW_FORWARD)
+            ],
+        )
+
+        self.btns.controls[0].disabled = True
 
         super().__init__()
 
@@ -617,18 +805,104 @@ class Order(UserControl):
     def close_modal(self):
         self.order_box.visible = False
         self.order_box.update()
-        sleep(0.75)
+        sleep(0.35)
 
         self.order.width = 0
         self.order.update()
-        sleep(0.35)
+        sleep(0.75)
+
+        self.page.controls.remove(self)
 
         self.main = MainPage()
         self.page.controls.insert(0, self.main)
         self.page.update()
         sleep(0.35)
 
+        self.main.open_mainpage()
+
+    def order_btn(self, label: str, btn_function, icon_selected):
+        return ElevatedButton(text=label,
+                              icon=icon_selected,
+                              on_click=btn_function)
+
+    def move_page(self, direction: str):
+
+        if direction == 'next':
+            self.page_number += 1
+
+        elif direction == 'previous':
+            self.page_number -= 1
+
+        if self.page_number <= 0:
+
+            self.page_number = 0
+            # Previous Btn
+            self.btns.controls[0].disabled = True
+            self.btns.controls[0].update()
+
+        elif self.page_number >= 5:
+            self.btns.controls[1].disabled = True
+            self.btns.controls[1].update()
+
+        else:
+            # Previous Btn
+            self.btns.controls[0].disabled = False
+            self.btns.controls[0].update()
+
+            # Next Btn
+            self.btns.controls[1].disabled = False
+            self.btns.controls[1].update()
+
+        start_index = self.page_size*self.page_number
+
+        self.order_list.controls[0].rows.clear()
+
+        for i in range(self.page_size):
+            row = DataRow(
+                cells=[
+                    DataCell(Text(start_index+i)),
+                    DataCell(Text("12/01/01")),
+                    DataCell(Text('Carlos')),
+                    DataCell(Text('Almeida')),
+                    DataCell(Text('R$ 100',)),
+                    DataCell(Text('1000 und')),
+                ]
+            )
+
+            self.order_list.controls[0].rows.append(row)
+
+        self.order_list.update()
+        
+
     def build(self):
+        self.page_size: int = 30
+        self.page_number: int = 0
+
+        container = Column(
+            expand=True,
+            horizontal_alignment=CrossAxisAlignment.CENTER,
+            alignment=MainAxisAlignment.CENTER
+        )
+
+        container.controls.append(self.title)
+        container.controls.append(self.new_order)
+        container.controls.append(self.order_list)
+
+        for i in range(self.page_size):
+            row = DataRow(
+                cells=[
+                    DataCell(Text(i+1)),
+                    DataCell(Text("12/01/01")),
+                    DataCell(Text('Carlos')),
+                    DataCell(Text('Almeida')),
+                    DataCell(Text('R$ 100',)),
+                    DataCell(Text('1000 und')),
+                ]
+            )
+            self.order_list.controls[0].rows.append(row)
+
+        container.controls.append(self.btns)
+        self.order_box.controls.append(container)
         self.order.content = self.order_box
         return self.order
 
@@ -653,13 +927,13 @@ def main(page: Page):
     # page.add(cashier)
     # cashier.open_modal()
 
-    product = Product()
-    page.add(product)
-    product.open_modal()
+    # product = Product()
+    # page.add(product)
+    # product.open_modal()
 
-    # order = Order()
-    # page.add(order)
-    # order.open_modal()
+    order = Order()
+    page.add(order)
+    order.open_modal()
 
     page.update()
 
